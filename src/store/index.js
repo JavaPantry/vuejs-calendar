6292cb6d814751c6cdb2b5f30f7ec4908463057a
Alexei Ptitchkin <ptitchkin@gmail.com>
2018-12-28 15:44:57 -0500

    154. Get event date in store

--- src/store/index.js
+++ src/store/index.js
@@ -16,7 +16,8 @@ export default new Vuex.Store({
       { description: 'Random event 1', date: moment('2018-12-06', 'YYYY-MM-DD') },
       { description: 'Random event 2', date: moment('2018-12-15', 'YYYY-MM-DD') },
       { description: 'Random event 3', date: moment('2018-12-28', 'YYYY-MM-DD') }
-    ]
+    ],
+    eventFormDate: null
   },
   mutations: {
     setCurrentMonth(state, payload) {
@@ -35,8 +36,11 @@ export default new Vuex.Store({
     addEvent(state, payload) {
       state.events.push({
         description: payload,
-        date: moment()
+        date: state.eventFormDate
       });
+    },
+    eventFormDate(state, payload) {
+      state.eventFormDate = payload
     }
   }
 });
