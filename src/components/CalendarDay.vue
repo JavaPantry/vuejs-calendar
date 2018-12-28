ac5e655ce6b9e661084f4ee4cfcf52593ba4caa7
Alexei Ptitchkin <ptitchkin@gmail.com>
2018-12-28 15:09:30 -0500

    151. Creating events state in store

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -11,12 +11,7 @@
         props: [ 'day' ],
         computed: {
             events() {
-                let mockData = [
-                    { description: 'Random event 1', date: this.$moment('2018-12-06', 'YYYY-MM-DD') },
-                    { description: 'Random event 2', date: this.$moment('2018-12-15', 'YYYY-MM-DD') },
-                    { description: 'Random event 3', date: this.$moment('2018-12-28', 'YYYY-MM-DD') }
-                ];
-                return mockData.filter(event => event.date.isSame(this.day, 'day'));
+                return this.$store.state.events.filter(event => event.date.isSame(this.day, 'day'));
             },
             classObject() {
                 let today = this.day.isSame(this.$moment(), 'day');
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,21 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 Vue.use(Vuex);
 
+import moment from 'moment-timezone';
+moment.tz.setDefault('UTC');
+
 export default new Vuex.Store({
   state: {
     currentYear: 2018,
     currentMonth: 12,
     eventFormPosX: 0,
     eventFormPosY: 0,
-    eventFormActive: false
+    eventFormActive: false,
+    events: [
+      { description: 'Random event 1', date: moment('2018-12-06', 'YYYY-MM-DD') },
+      { description: 'Random event 2', date: moment('2018-12-15', 'YYYY-MM-DD') },
+      { description: 'Random event 3', date: moment('2018-12-28', 'YYYY-MM-DD') }
+    ]
   },
   mutations: {
     setCurrentMonth(state, payload) {
